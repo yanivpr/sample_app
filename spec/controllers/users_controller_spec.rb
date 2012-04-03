c93733fc1a6efafcc7e740dbe2b0300fde065f3c
Yaniv Preiss <yanivpr@gmail.com>
2012-04-03 22:52:31 +0200

    Added edit page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,7 +15,7 @@ describe UsersController do
     end
   end
 
-  describe "Get 'show'" do
+  describe "GET 'show'" do
    before(:each) do
 	   @user = Factory(:user)
    end
@@ -24,7 +24,7 @@ describe UsersController do
 	    get :show, :id => @user
 	    response.should be_success
     end
-    it "should find the right user" do
+    it "should find the right integration_test_sign_in(user)user" do
 	    get :show, :id => @user
 	    assigns(:user).should == @user
     end
@@ -46,7 +46,32 @@ describe UsersController do
 
   end
 
-  describe "Post 'create'" do
+  describe "GET 'edit'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    it "should be successful" do
+     get :edit, :id => @user
+     response.should be_success
+    end
+
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+
+    it "should have a link to change the Gravatar" do
+      get :edit, :id => @user
+      gravatar_url = "http://gravatar.com/emails"
+      response.should have_selector("a", :href => gravatar_url, :content => "change")
+    end
+    
+    
+  end
+
+  describe "POST 'create'" do
 	describe "failure" do
 		
 		before(:each) do
@@ -102,4 +127,53 @@ describe UsersController do
 
   end
 
+  describe "PUT 'update'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+  
+    describe "failure" do
+    
+      before(:each) do
+        @attr = { :email => "", :name => "", :password => "", :password_confirmation => "" }
+      end
+      
+      it "should render the 'edit' page" do
+        put :update, :id => @user, :user => @attr
+        response.should render_template('edit')
+      end
+
+      it "should have the right title" do
+        put :update, :id => @user, :user => @attr
+        response.should have_selector("title", :content => "Edit user")
+      end
+
+    end
+  
+    describe "success" do
+      
+      before(:each) do
+        @attr = { :name => "New Name", :email => "user@example.org", 
+		:password => "barbaz", :password_confirmation => "barbaz" }
+      end
+       
+      it "should change the user's attributes" do
+        put :update, :id => @user, :user => @attr
+        @user.reload
+        @user.name.should == @attr[:name]
+        @user.email.should == @attr[:email]
+      end
+  
+      it "should redirect to the user show page" do
+        put :update, :id => @user, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+     
+      it "should have a flash message" do
+        put :update, :id => @user, :user => @attr
+        flash[:success].should =~ /updated/
+      end
+    end
+  end
 end
